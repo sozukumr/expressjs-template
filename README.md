de2483472b146da19945cd411bf0a5441de66bf7
Sonu Kumar <sonukumarsuman365@gmail.com>
2024-09-28 08:48:09 +0530

    upload

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # expressjs-template
  
+Directory Structure:
+```
+node_modules/
+app.js
+public/
+images/
+stylesheets/
+javascripts/
+views/
+    header.ejs
+    footer.ejs
+    index.ejs
+    layout.ejs
+routes/
+    index.js
+    users.js
+models/
+    user.js
+package.json
+```
---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
