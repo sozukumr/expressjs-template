69e9bf0c4a3ea7a1144a82e3227cbfc5baca06f4
Sonu Kumar <sonukumarsuman365@gmail.com>
2024-09-28 09:16:04 +0530

    bug fixes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const ejs = require('ejs');
 
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Home' });
+router.get('/', async (req, res) => {
+  const content = await ejs.renderFile('views/content/home.ejs', {});
+  res.render('layout', {
+    title: 'Home',
+    content: content
+  });
+});
+
+router.get('/about', async (req, res) => {
+  const content = await ejs.renderFile('views/content/about.ejs', {});
+  res.render('layout', {
+    title: 'About',
+    content: content
+  });
+});
+
+router.get('/contact', async (req, res) => {
+  const content = await ejs.renderFile('views/content/contact.ejs', {});
+  res.render('layout', {
+    title: 'Contact',
+    content: content
+  });
 });
 
 module.exports = router;
